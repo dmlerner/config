10cacff90158a230ddcef554d069910910542ef3
David Lerner <dmlerner@gmail.com>
2020-01-25 09:28:24 -0800

    link function

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
-cd $HOME
-CONFIG=$HOME/gdrive/config
-DOTFILES=$CONFIG/dotfiles
-SCRIPTS=$CONFIG/scripts
+
 sudo apt install neovim i3 fonts-powerline zsh xcalib thefuck python3-distutils colordiff google-rebaser ranger ack-grep nodejs npm yarn vim-google-config python3-pip xdotool
-mkdir -p $HOME/.config/i3
-sudo rm -rf $HOME/.config/i3/config
-ln -sf $DOTFILES/i3config $HOME/.config/i3/config
-mkdir -p $HOME/.config/nvim
-sudo rm -rf $HOME/.config/nvim/init.vim
-ln -sf $DOTFILES/nvimrc $HOME/.config/nvim/init.vim
-sudo rm -rf $HOME/.xsessionrc
-ln -sf $DOTFILES/xsessionrc $HOME/.xsessionrc
-sudo rm -rf $HOME/.oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's/exec/#exec/')"
-sudo rm -rf $HOME/.vimrc
-ln -sf $DOTFILES/google-desktop-vimrc $HOME/.vimrc
-sudo rm -rf $HOME/.zshenv
-ln -sf $DOTFILES/zshenv $HOME/.zshenv
-sudo rm -rf $HOME/.zshrc
-ln -sf $DOTFILES/zshrc $HOME/.zshrc
-sudo rm -rf $HOME/.config/alacritty
-mkdir -p $HOME/.config/alacritty
-ln -sf $DOTFILES/alacritty.yml $HOME/.config/alacritty
-sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
-chmod +x $DOTFILES/.git/hooks/*
-chmod +x $SCRIPTS/.git/hooks/*
+echo
 pip3 install --user pynvim jedi numpy pandas matplotlib
+echo
+
+cd $HOME
+
+GDRIVE=$HOME/gdrive
+DOTFILES=$GDRIVE/config/dotfiles
+SOURCE_CONFIG=$GDRIVE/config
+DEST_CONFIG=$HOME/.config
+chmod +x $SOURCE_CONFIG/.git/hooks/*
+
+link () {
+  local from=$DOTFILES/$1
+  local to=$2
+  echo $from $to
+  mkdir -p $(dirname $to)
+  ln -sf $from $to
+}
+link_home() { link $1 $HOME/$2; }
+link_config() { link $1 $DEST_CONFIG/$2; }
+
+link_config drivefs Google/DriveFS/preferences.json
+
+if [[ ! -a $HOME/.oh-my-zsh ]]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's/exec/#exec/')"
+  rm -f $HOME/install.sh
+fi
+
+if [[ ! -a $HOME/.oh-my-zsh/custom/themes/powerlevel10k ]]; then
+  sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+fi
+
+link_home xsessionrc .xsessionrc
+link_home zshenv .zshenv
+link_home zshrc .zshrc
+link_home hgrc .hgrc
+
+link_config alacritty.yml alacritty/alacritty.yml
+link_config i3status i3status/config
+link_config i3config i3/config
+i3-msg restart
+
+link_home vimrc .vimrc
+link_config nvimrc nvim/init.vim
 vim -c ':PlugInstall' -c ':q' -c ':q'
-sudo rm -rf $HOME/.config/i3status/config
-mkdir -p $HOME/.config/i3status
-ln -sf $DOTFILES/i3status $HOME/.config/i3status/config
